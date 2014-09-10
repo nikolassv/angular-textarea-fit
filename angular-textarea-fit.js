d63e59b71d82fd94c44440f15a15991fe23ab08a
Nikolas Schmidt-Voigt <nikolas.schmidt-voigt@posteo.de>
2014-09-11 00:27:09 +0200

    Updated error message

--- angular-textarea-fit.js
+++ angular-textarea-fit.js
@@ -65,7 +65,7 @@ angular
           if (!angular.isFunction($element.height)) {
             $log.error('textareaFit directive only works when jQuery is loaded');
           } else if ($element.is('textarea')) {
-            $log.info('textareaFit directive only works for elements of type "textareo"');
+            $log.info('textareaFit directive only works for elements of type "textarea"');
           } else {
             var elClone = angular.element('<div>'),
                 setEqualHeight = function () {
